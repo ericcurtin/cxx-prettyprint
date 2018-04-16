2db8f4f0a30536f4f3748631ed5e8270076c4c79
Eric Curtin <ericcurtin17@gmail.com>
2018-04-16 23:03:24 +0100

    Create prettyprint.hpp that knows whether compiler is C++11 or not

--- ppdemo.cpp
+++ ppdemo.cpp
@@ -19,21 +19,23 @@
 
 #include "prettyprint.hpp"
 
-
 /* Customization option 1: Direct partial/full specialization.
    Here we specialize for std::vector<double>.
 */
-template<> const pretty_print::delimiters_values<char> pretty_print::delimiters<std::vector<double>, char>::values = { "|| ", " : ", " ||" };
+template <>
+const pretty_print::delimiters_values<char>
+    pretty_print::delimiters<std::vector<double>, char>::values = {"|| ", " : ",
+                                                                   " ||"};
 
 /* Customization option 2: Create a delimiters class for later use and reuse. */
-struct MyDelims { static const pretty_print::delimiters_values<char> values; };
-const pretty_print::delimiters_values<char> MyDelims::values = { "<", "; ", ">" };
-
+struct MyDelims {
+  static const pretty_print::delimiters_values<char> values;
+};
+const pretty_print::delimiters_values<char> MyDelims::values = {"<", "; ", ">"};
 
 /* Demo: run with a couple of command-line arguments. */
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
   std::string cs;
   std::unordered_map<int, std::string> um;
   std::map<int, std::string> om;
@@ -48,8 +50,7 @@ int main(int argc, char * argv[])
 
   std::cout << "Printing pairs." << std::endl;
 
-  for (int i = 1; i < argc; ++i)
-  {
+  for (int i = 1; i < argc; ++i) {
     std::string s(argv[i]);
     std::pair<int, std::string> p(i, s);
 
@@ -58,7 +59,7 @@ int main(int argc, char * argv[])
     v.push_back(s);
     vv.push_back(v);
     vp.push_back(p);
-    vd.push_back(1./double(i));
+    vd.push_back(1. / double(i));
     ss.insert(s);
     cs += s;
 
@@ -68,25 +69,34 @@ int main(int argc, char * argv[])
 
   /* Demo: pretty-printing for various containers. */
 
-  std::array<char, 5> a{{ 'h', 'e', 'l', 'l', 'o' }};
-  std::valarray<double> va { 1.0, -0.5, 0.25, -0.125 };
+  std::array<char, 5> a{{'h', 'e', 'l', 'l', 'o'}};
+  std::valarray<double> va{1.0, -0.5, 0.25, -0.125};
 
-  std::cout << "Vector: " << v << std::endl                // vector of strings
-            << "Incremental vector: " << vv << std::endl   // nestes vector of vectors
-            << "Pairs: " << vp << std::endl                // vector of pairs
-            << "Another vector: " << vd << std::endl       // vector of doubles using Customization #1
-            << "Set: " << ss << std::endl                  // set of strings, using partially specialized default set delimiters
-            << "OMap: " << om << std::endl                 // associative container (value type is a std::pair)
-            << "UMap: " << um << std::endl                 // ditto
-            << "String: " << cs << std::endl               // just a plain string, note that std::string has begin()/end()
-            << "Array: " << a << std::endl                 // an std::array
-            << "Valarray: " << va << std::endl              // an std::valarray
-  ;
+  std::cout << "Vector: " << v << std::endl  // vector of strings
+            << "Incremental vector: " << vv
+            << std::endl                     // nestes vector of vectors
+            << "Pairs: " << vp << std::endl  // vector of pairs
+            << "Another vector: " << vd
+            << std::endl  // vector of doubles using Customization #1
+            << "Set: " << ss << std::endl  // set of strings, using partially
+                                           // specialized default set delimiters
+            << "OMap: " << om
+            << std::endl  // associative container (value type is a std::pair)
+            << "UMap: " << um << std::endl    // ditto
+            << "String: " << cs << std::endl  // just a plain string, note that
+                                              // std::string has begin()/end()
+            << "Array: " << a << std::endl    // an std::array
+            << "Valarray: " << va << std::endl  // an std::valarray
+      ;
 
-  /* Demo: Here we use our reusable delimiter class MyDelims by directly accessing some interna. */
-  std::cout << "Reusable delimiters: "
-            << pretty_print::print_container_helper<std::vector<std::string>, char, std::char_traits<char>, MyDelims>(v)
-            << std::endl;
+  /* Demo: Here we use our reusable delimiter class MyDelims by directly
+   * accessing some interna. */
+  std::cout
+      << "Reusable delimiters: "
+      << pretty_print::print_container_helper<std::vector<std::string>, char,
+                                              std::char_traits<char>, MyDelims>(
+             v)
+      << std::endl;
 
   /* Demo: We can pretty-print std::pair and std::tuple.
      (You already saw pairs in the associative containers above.)
@@ -94,7 +104,8 @@ int main(int argc, char * argv[])
   auto a1 = std::make_pair(std::string("Jello"), 9);
   auto a2 = std::make_tuple(1729);
   auto a3 = std::make_tuple("Qrgh", a1, 11);
-  auto a4 = std::make_tuple(1729, 2875, std::pair<double, std::string>(1.5, "abc"));
+  auto a4 =
+      std::make_tuple(1729, 2875, std::pair<double, std::string>(1.5, "abc"));
   std::tuple<> a5;
 
   /* Demo: Here we achieve the same using a type-erasing helper class. */
@@ -106,17 +117,17 @@ int main(int argc, char * argv[])
             << pretty_print::custom_delims<MyDelims>(a3) << std::endl;
 
   /* Demo: raw arrays can be printed with a helper wrapper. */
-  int arr[] = { 1, 4, 9, 16 };
-  int err[] = { 2 };
+  int arr[] = {1, 4, 9, 16};
+  int err[] = {2};
 
   std::cout << "Static C array: " << arr << std::endl
             << "Static C array: " << err << std::endl
-            << "Static C array with length: " << pretty_print_array(arr + 1, 2) << std::endl
+            << "Static C array with length: " << pretty_print_array(arr + 1, 2)
+            << std::endl
             << "Pair:    " << a1 << std::endl
             << "0-tuple: " << a5 << std::endl
             << "1-tuple: " << a2 << std::endl
             << "n-tuple: " << a3 << std::endl
             << "n-tuple: " << a4 << std::endl
-            << "Hashmap bucket: " << bucket_print(um, 0) << std::endl
-  ;
+            << "Hashmap bucket: " << bucket_print(um, 0) << std::endl;
 }
